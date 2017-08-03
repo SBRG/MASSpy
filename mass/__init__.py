14d8b3ace2f1f7b982e00efad2879d0a4c8cf70e
z-haiman <zhaiman@eng.ucsd.edu>
2017-08-03 14:06:23 -0700

    Added version

--- mass/__init__.py
+++ mass/__init__.py
@@ -3,9 +3,9 @@
 from __future__ import absolute_import
 
 from mass.test import (
-    test_MassMetabolite, test_MassReaction
-    )
+    test_MassMetabolite, test_MassReaction)
 
 from mass.core import (
-    MassMetabolite, MassReaction, MassModel
-    )
+    MassMetabolite, MassReaction, MassModel)
+
+__version__ = "0.1.0"
