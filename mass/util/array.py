b96bdcd24bde2d6970ee9a26a8688560950d5565
siddC <github.chauhan.siddharth@gmail.com>
2017-08-15 16:46:21 -0700

    changed nullspace and left_snullspace functions. also added rationalize options

--- mass/util/array.py
+++ mass/util/array.py
@@ -84,99 +84,45 @@ def create_stoichiometric_matrix(model, matrix_type=None, dtype=None,
 
 	return s_matrix
 
-def nullspace(A, atol=1e-13, rtol=0):
-	"""Compute an approximate basis for the nullspace of A.
-	The algorithm used by this function is based on the singular value
-	decomposition of `A`.
+def nullspace(A, orient="row", rationalize=False):
+    if isinstance(A, np.ndarray) or isinstance(A, pd.DataFrame):
+        pass
+    elif isinstance(A, dok_matrix) or isinstance(A, lil_matrix):
+        A = A.toarray()
+    else:
+        raise TypeError("Matrix must be one of the following formats: "
+                "numpy.ndarray, dok_matrix, lil_matrix, or pandas 'DataFrame'")
+        
+    if orient not in ("row" "col"):
+        raise TypeError("Orientation must be either 'row' or 'col'")
+                        
+    A = np.atleast_2d(A)
+    ns = np.array(Matrix(A).nullspace()).astype(np.float64)
+    # Rationalize if True
+    if rationalize:
+        for i in range(ns.shape[0]):
+            # Find abs min value with index
+            ma = np.ma.masked_values(np.absolute(ns[i]), 0.0, copy=False)
+            # Rationalize by dividing by abs min value
+            ns[i] = np.divide(ns[i], ma.min())
+            
+    # Set orientation
+    if orient is "col":
+        ns = ns.T
 
-	Parameters
-	----------
-	A : numpy.ndarray, scipy.sparse dok matrix or lil matrix, pandas
-		A should be at most 2-D.  A 1-D array with length k will be treated
-		as a 2-D with shape (1, k)
-	atol : float
-		The absolute tolerance for a zero singular value.  Singular values
-		smaller than `atol` are considered to be zero.
-	rtol : float
-		The relative tolerance.  Singular values less than rtol*smax are
-		considered to be zero, where smax is the largest singular value.
+    return ns
 
-	If both `atol` and `rtol` are positive, the combined tolerance is the
-	maximum of the two; that is::
-	tol = max(atol, rtol * smax)
-	Singular values smaller than `tol` are considered to be zero.
-
-	Returns
-	-------
-	numpy.ndarray
-		If `A` is an array with shape (m, k), then `ns` will be an array
-		with shape (k, n), where n is the estimated dimension of the
-		nullspace of `A`.  The columns of `ns` are a basis for the
-		nullspace; each element in numpy.dot(A, ns) will be approximately
-		zero.
-
-	Notes
-	-----
-	Algorithm from the numpy cookbook.
-	"""
-	if isinstance(A, np.ndarray) or isinstance(A, pd.DataFrame):
-		pass
-	elif isinstance(A, dok_matrix) or isinstance(A, lil_matrix):
-		A = A.toarray()
-	else:
-		raise TypeError("Matrix must be one of the following formats: "
-				"numpy.ndarray, dok_matrix, lil_matrix, or pandas 'DataFrame'")
-
-	A = np.atleast_2d(A)
-	u, s, vh = np.linalg.svd(A)
-	tol = max(atol, rtol * s[0])
-	nnz = (s >= tol).sum()
-	ns = vh[nnz:].conj().T
-	return ns
-
-
-def left_nullspace(A, atol=1e-13, rtol=0):
-	"""Compute an approximate basis for the left nullspace of A.
-	The algorithm used by this function is based on the singular value
-	decomposition of `A`.
-
-	Parameters
-	----------
-	A : numpy.ndarray, dok_matrix, lil_matrix, or pandas 'DataFrame'
-		A should be at most 2-D.  A 1-D array with length k will be treated
-		as a 2-D with shape (1, k)
-	atol : float
-		The absolute tolerance for a zero singular value.  Singular values
-		smaller than `atol` are considered to be zero.
-	rtol : float
-		The relative tolerance.  Singular values less than rtol*smax are
-		considered to be zero, where smax is the largest singular value.
-
-	If both `atol` and `rtol` are positive, the combined tolerance is the
-	maximum of the two; that is::
-	tol = max(atol, rtol * smax)
-	Singular values smaller than `tol` are considered to be zero.
-
-	Returns
-	-------
-	numpy.ndarray
-		If `A` is an array with shape (m, k), then `ns` will be an array
-		with shape (m, n), where n is the estimated dimension of the
-		left nullspace of `A`.  The columns of `lns` are a basis for the
-		left nullspace; each element in numpy.dot(np.transpose(A), lns) will
-		be approximately zero.
-	"""
-	if isinstance(A, np.ndarray) or isinstance(A, pd.DataFrame):
-		pass
-	elif isinstance(A, dok_matrix) or isinstance(A, lil_matrix):
-		A = A.toarray()
-	else:
-		raise TypeError("Matrix must be one of the following formats: "
-				"numpy.ndarray, dok_matrix, lil_matrix, or pandas 'DataFrame'")
-
-	At = np.transpose(A)
-	lns = nullspace(At, atol=atol, rtol=rtol)
-	return lns
+def left_nullspace(A, orient="row", rationalize=False):
+    if isinstance(A, np.ndarray) or isinstance(A, pd.DataFrame):
+        pass
+    elif isinstance(A, dok_matrix) or isinstance(A, lil_matrix):
+        A = A.toarray()
+    else:
+        raise TypeError("Matrix must be one of the following formats: "
+                "numpy.ndarray, dok_matrix, lil_matrix, or pandas 'DataFrame'")
+    A = np.atleast_2d(A)
+    lns = nullspace(np.transpose(A), orient, rationalize)
+    return lns
 
 def matrix_rank(A, tol=None):
 	"""Get the rank of a matrix.
