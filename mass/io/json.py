c47dba5df3da292b1c33289f1058c308680e1094
siddC <github.chauhan.siddharth@gmail.com>
2017-08-29 09:41:00 -0700

    Added save and load methods, allowing for json loading/saving directly to/from file on disk

--- mass/io/json.py
+++ mass/io/json.py
@@ -378,28 +378,69 @@ def from_json(document):
     """
     return _model_from_dict(json.loads(document))
 
+def save_json_model(model, filename, pretty=False, **kwargs):
+    """
+    Write the mass model to a file in JSON format.
 
+    ``kwargs`` are passed on to ``json.dump``.
 
+    Parameters
+    ----------
+    model : mass.Model
+        The mass model to represent.
+    filename : str or file-like
+        File path or descriptor that the JSON representation should be
+        written to.
+    pretty : bool, optional
+        Whether to format the JSON more compactly (default) or in a more
+        verbose but easier to read fashion. Can be partially overwritten by the
+        ``kwargs``.
 
+    See Also
+    --------
+    to_json : Return a string representation.
+    json.dump : Base function.
+    """
+    obj = _model_to_dict(model)
+    obj[u"version"] = JSON_SPEC
 
+    if pretty:
+        dump_opts = {
+            "indent": 4, "separators": (",", ": "), "sort_keys": True,
+            "allow_nan": False}
+    else:
+        dump_opts = {
+            "indent": 0, "separators": (",", ":"), "sort_keys": False,
+            "allow_nan": False}
+    dump_opts.update(**kwargs)
 
+    if isinstance(filename, string_types):
+        with open(filename, "w") as file_handle:
+            json.dump(obj, file_handle, **dump_opts)
+    else:
+        json.dump(obj, filename, **dump_opts)
 
+def load_json_model(filename):
+    """
+    Load a mass model from a file in JSON format.
 
+    Parameters
+    ----------
+    filename : str or file-like
+        File path or descriptor that contains the JSON document describing the
+        mass model.
 
+    Returns
+    -------
+    mass.Model
+        The cobra model as represented in the JSON document.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    See Also
+    --------
+    from_json : Load from a string.
+    """
+    if isinstance(filename, string_types):
+        with open(filename, "r") as file_handle:
+            return _model_from_dict(json.load(file_handle))
+    else:
+        return _model_from_dict(json.load(filename))
