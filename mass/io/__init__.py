d64dfd8dfe21cd6139cb7eba073813c3d7900d19
z-haiman <zhaiman@eng.ucsd.edu>
2017-10-04 16:45:09 -0700

    Initialization of io, only certain functions now initialized on from mass.io import *

--- mass/io/__init__.py
+++ mass/io/__init__.py
@@ -2,9 +2,12 @@
 
 from __future__ import absolute_import
 
-from mass.io.json import *
-from mass.io.massef import *
+from mass.io.json import (
+    write_json_model, read_json_model)
+from mass.io.massef import (
+    create_enzyme_model)
 try:
-    from mass.io.sbml import *
+    from mass.io.sbml import (
+        write_sbml_model, read_sbml_model)
 except:
     pass
