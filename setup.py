367768ed2bd72fb0a0d74c2fbd96ae13e7c9c786
z-haiman <zhaiman@eng.ucsd.edu>
2018-02-08 13:36:24 -0800

    Bump version: 0.1.0a12 → 0.1.0a13

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except IOError:
 
 setup(
 	name="masspy",
-	version="0.1.0a12",
+	version="0.1.0a13",
 	description="MASSpy is a package for kinetic modeling and simulation of "
 				"biological networks",
 	license="LGPL/GPL v2+",
