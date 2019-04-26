e8931ff68234ace1562f0ba11e7de323c7bae237
z-haiman <zhaiman@eng.ucsd.edu>
2019-04-26 16:16:01 -0700

    Bump version: 0.1.0a35 → 0.1.0a36

--- mass/__init__.py
+++ mass/__init__.py
@@ -18,7 +18,7 @@ from mass.core import (
 from mass.util import (
     is_simulatable, qcqa_model, qcqa_simulation, show_versions, strip_time)
 
-__version__ = "0.1.0a35"
+__version__ = "0.1.0a36"
 
 # set the warning format to be prettier and fit on one line
 _mass_path = _dirname(_abspath(__file__))
