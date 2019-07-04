254c866b40bfab49ef7e4e468446ab73aea5061c
z-haiman <zhaiman@eng.ucsd.edu>
2019-07-04 12:04:30 -0700

    Added master_doc as index

--- documentation_builder/conf.py
+++ documentation_builder/conf.py
@@ -4,7 +4,7 @@
 # list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
-# -- Path setup --------------------------------------------------------------
+# -- Path setup ---------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
-# -- Project information -----------------------------------------------------
+# -- Project information ------------------------------------------------------
 
 project = 'masspy'
 copyright = '2019, Z. Haiman'
@@ -25,7 +25,7 @@ author = 'Z. Haiman'
 release = '0.1.0a38'
 
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration ----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -42,8 +42,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
+# The master toctree document.
+master_doc = 'index'
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output --------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -54,3 +56,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Options for LaTeX output -------------------------------------------------
+
+# -- Options for manual page output -------------------------------------------
+
+# -- Options for Texinfo output -----------------------------------------------
