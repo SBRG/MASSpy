b1ed13cbf135e3882f5e94f1a15a4b3c7f7ecf65
z-haiman <zhaiman@eng.ucsd.edu>
2020-09-29 23:24:28 -0700

    WIP: docker entrypoint script, dockerfile and related

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
+
 set -e
 
-if [ "${1:0:1}" = '-' ]; then
-    set -- gurobi "$@"
+# Set verbose and debug options
+if [ -n '${VERBOSE}' ] ; then 
+    case ${VERBOSE} in 
+        0) set +v && set +x ;;
+        1) set -v ;;
+        2) set -x ;;
+    esac
 fi
 
-if [[ "$VERBOSE" = "yes" ]]; then
-    set -x
-fi
+# Move client token license 
 
-LICENSE=/usr/src/opt/licenses/gurobi.lic
-if [ -f $LICENSE ]; then
-    echo "Skipping license creation"
-else
-    echo "Configure license $GUROBI_LICENSE"
-fi
+# # Configure gurobi license
+# if [ ! -z ${GRB_LICENSE_FILE} ] && [ -f ${GRB_LICENSE_FILE} ]; then 
+#     echo 'Gurobi license exists at '${GRB_LICENSE_FILE}
+# elif [ ! -z ${GRB_LICENSE_FILE} ] ; then
+#     echo 'Creating token server client license'
+# else
+#     echo 'No Gurobi license available.'
+# fi
+
+# # Ensures license path variable points to license volume if not set
+# if [ -z ${GRB_LICENSE_FILE} ] ; then \
+#     export GRB_LICENSE_FILE=${HOME}/opt/licenses/gurobi.lic
+# fi
+
+exec "$@"
