08f316b55f913cfc5ca0e513a80b7087f39ae898
z-haiman <zhaiman@eng.ucsd.edu>
2020-10-07 15:50:55 -0700

    Docker entrypoint script update and added pyinstall-solver helper script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,28 +3,38 @@
 set -e
 
 # Set verbose and debug options
-if [ -n '${VERBOSE}' ] ; then 
-    case ${VERBOSE} in 
-        0) set +v && set +x ;;
-        1) set -v ;;
-        2) set -x ;;
+if [ -n '${VERBOSE}' ] ; then
+    case ${VERBOSE} in
+        [0-1]) set +v && set +x ;;
+        # For extra verbosity/debugging purposes
+        2) set -v ;;
+        3) set -x ;;
     esac
 fi
 
-# Move client token license 
+# Set expected license directory
+license_dir=${HOME}/opt/licenses
+# Let user know which optimizer licenses, and by extension, 
+# which optimizers are available in container. 
+for solver in 'CPLEX' 'Gurobi' ; do
+    case $solver in
+        'CPLEX')
+            solver_lic_path=$license_dir/CPLEX ;;
+        'Gurobi')
+            solver_lic_path=$license_dir/gurobi.lic ;;
+    esac
+    if [ ${VERBOSE} -ne 0 ] ; then
+        if [ -e $solver_lic_path ] ; then
+            echo "$solver license exists at $solver_lic_path" ;
+        else
+            echo "No $solver license available" ;
+        fi ;
+    fi ;
+    done
 
-# # Configure gurobi license
-# if [ ! -z ${GRB_LICENSE_FILE} ] && [ -f ${GRB_LICENSE_FILE} ]; then 
-#     echo 'Gurobi license exists at '${GRB_LICENSE_FILE}
-# elif [ ! -z ${GRB_LICENSE_FILE} ] ; then
-#     echo 'Creating token server client license'
-# else
-#     echo 'No Gurobi license available.'
-# fi
-
-# # Ensures license path variable points to license volume if not set
-# if [ -z ${GRB_LICENSE_FILE} ] ; then \
-#     export GRB_LICENSE_FILE=${HOME}/opt/licenses/gurobi.lic
-# fi
-
+# If Gurobi avalable, set 'GRB_LICENSE_FILE' to point to license
+if [ -e $license_dir/gurobi.lic ] ; then
+    export GRB_LICENSE_FILE=$license_dir/gurobi.lic ;
+fi
+    
 exec "$@"
