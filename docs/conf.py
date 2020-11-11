2bc4b24e10eb6f9785ae1acf575247aa0ee01db2
z-haiman <zhaiman@eng.ucsd.edu>
2020-11-11 14:13:54 -0800

    Updated setup to follow PEP guidelines and depend primarily on setup.cfg
    Updated relevant files with new setup changes
    
    Set workflows to activate only on final_release
    
    Allow merge to master branch to attempt to deploy package to testpypi

--- docs/conf.py
+++ docs/conf.py
@@ -10,23 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute.
 
-import os
 import sys
-from os.path import dirname
+import os
 
-sys.path.insert(0, dirname(dirname(__file__)))
+SRC_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "src")
+sys.path.insert(0, SRC_PATH)
 
-from mass import __version__  # noqa: E402
-
-# -- Project information ------------------------------------------------------
-
-project = 'MASSpy'
-copyright = '2019, Z. Haiman'
-author = 'Z. Haiman'
-
-# The full version, including alpha/beta/rc tags
-release = __version__
-version = '.'.join(release.split('.')[:2])
 
 # -- General configuration ----------------------------------------------------
 
@@ -46,25 +35,24 @@ extensions = [
     'sphinxcontrib.bibtex',
 ]
 
-# Automated documention of Python Code
+# Automated documention of Python Code (autoapi)
 autoapi_type = 'python'
-autoapi_dirs = ['../mass']
-autoapi_ignore = [
-    '.tox', '.pytest_cache', 'scripts', 'benchmarks', "notebooks"]
+autoapi_dirs = [SRC_PATH]
 # Keep files for documentation preview in IDEs
 autoapi_keep_files = True
-
+# Automated section labeling (autosectionlabel)
 autosectionlabel_prefix_document = True
 autodoc_mock_imports = [
     "cobra",
     "depinfo",
     "escher",
-    "gurobipy",
     "libroadrunner",
     "matplotlib",
     "numpy",
+    "optlang",
     "pandas",
     "scipy",
+    "six",
     "sympy",
     "tabulate"
 ]
@@ -73,23 +61,29 @@ autodoc_mock_imports = [
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# The master toctree document.
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '**.ipynb_checkpoints', 'Thumbs.db', '.DS_Store']
-
-# The master toctree document.
-master_doc = 'index'
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 pygments_style = 'sphinx'
 
-nbsphinx_timeout = 180
+# -- Project information ------------------------------------------------------
+
+project = 'MASSpy'
+copyright = '2019, Z. Haiman'
+author = 'Z. Haiman'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+# This import has to be here.
+from mass import __version__ as release  # noqa: E402
+
+version = ".".join(release.split(".")[:2])
 
 # -- Options for HTML output --------------------------------------------------
 
@@ -105,11 +99,6 @@ if not on_rtd:
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
 # A list of paths that contain extra files not directly related to the
 # documentation, such as robots.txt or .htaccess. Relative paths are taken as
 # relative to the configuration directory. They are copied to the output
@@ -118,21 +107,54 @@ html_extra_path = ["robots.txt"]
 
 # -- Options for LaTeX output -------------------------------------------------
 
+latex_elements = {
+    "papersize": "a4paper",
+    "preamble": r"\usepackage{amsmath,amssymb}",
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'MASSpy.tex', u'MASSpy Documentation',
-   u'Zachary Haiman', 'manual'),
+  (
+      'index',
+      'MASSpy.tex',
+      u'MASSpy Documentation',
+      u'Z. Haiman',
+      'manual'
+    ),
 ]
 
 
 # -- Options for manual page output -------------------------------------------
 
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'MASSpy', u'MASSpy Documentation',
-     [u'Zachary Haiman'], 1)
+    (
+        'index',
+        'MASSpy',
+        u'MASSpy Documentation',
+        [u'Z. Haiman'],
+        1
+    )
 ]
 
 # -- Options for Texinfo output -----------------------------------------------
 
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (
+        "index",
+        "MASSpy",
+        "MASSpy Documentation",
+        "Z. Haiman",
+        "MASSpy",
+        "A package for dynamic modeling of biological processes",
+        "Miscellaneous",
+    )
+]
 
 # -- NBSphinx -----------------------------------------------------------------
 
@@ -141,6 +163,7 @@ nbsphinx_execute = 'never'
 nbsphinx_execute_arguments = [
     "--Application.log_level=CRITICAL",
 ]
+nbsphinx_timeout = 180
 
 # -- Intersphinx --------------------------------------------------------------
 
