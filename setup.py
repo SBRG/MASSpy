c91cbd5b9152831eef7fc975dc18bebd46dfa6e7
z-haiman <zhaiman@eng.ucsd.edu>
2021-05-04 13:25:45 -0700

    OSI classifier fix

--- setup.py
+++ setup.py
@@ -74,7 +74,6 @@ if __name__ == "__main__":
             'Intended Audience :: Developers',
             'Topic :: Software Development :: Build Tools',
             'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT',
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
