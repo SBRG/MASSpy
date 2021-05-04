4ecf1831ed275d74397cada573cec8001001b080
z-haiman <zhaiman@eng.ucsd.edu>
2021-05-04 13:23:16 -0700

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except IOError:
 if __name__ == "__main__":
     setup(
         name="masspy",
-        version="0.1.1",
+        version="0.1.2",
         packages=find_packages(),
         setup_requires=setup_requirements,
         install_requires=[
