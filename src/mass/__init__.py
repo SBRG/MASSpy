b7740a1e3b9c63df7f53a185a468c408b2fe022b
z-haiman <zhaiman@eng.ucsd.edu>
2021-11-19 17:29:40 -0800

    version correction

--- src/mass/__init__.py
+++ src/mass/__init__.py
@@ -18,7 +18,7 @@ from mass.simulation import Simulation
 from mass.util import qcqa_model, show_versions, strip_time
 
 
-__version__ = "0.1.4.post0"
+__version__ = "0.1.5b0"
 
 # set the warning format to be prettier and fit on one line
 _MASS_PATH = _dirname(_abspath(__file__))
